7e359a91babf396527f04b9a77b0a91b69d64bcc
BlueMoonJune <bluemoonjune0403@gmail.com>
2026-04-23 13:41:14 -0700

    installer

---
+++ install.lua
@@ -0,0 +1,4 @@
+shell.run("wget https://raw.githubusercontent.com/davidm/lua-matrix/refs/heads/master/lua/matrix.lua")
+shell.run("wget https://raw.githubusercontent.com/BlueMoonJune/mechatronics/refs/heads/master/gpsx.lua")
+shell.run("wget https://raw.githubusercontent.com/BlueMoonJune/mechatronics/refs/heads/master/orient.lua")
+shell.run("wget https://raw.githubusercontent.com/BlueMoonJune/mechatronics/refs/heads/master/pid.lua")
